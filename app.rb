fdbec432ce64f0df93d31fbb09e257f40fd5db7f
Matthew McCormack <mmccormack1182@gmail.com>
2019-02-07 14:01:47 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @new_text = PigLatinizer.new.piglatinize(params[:user_phrase])
+  end
+
+
 end
+
+
+"eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay eamStray andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay"
+
+"eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay ayStream andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay"
