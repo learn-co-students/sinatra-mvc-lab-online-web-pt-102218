82c92915a960328841450665dd715eb460ffe97f
Linda Chang <lindachang186@gmail.com>
2019-02-17 17:32:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,8 +6,12 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/piglatinize' do 
+  post '/piglatinize' do
+    @new_phrase = PigLatinizer.new.piglatinize(params[:user_phrase])
 
   end
 
 end
+
+"eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay ayStream andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay"
+"eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay ayStream andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay"
